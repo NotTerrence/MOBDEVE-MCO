21a72b53c7ab051fd21a09ead4f44a69dc39a72a
NotTerrence <terencedeindugang@gmail.com>
2021-12-11 21:27:19 +0800

    Added OptionActivity and GameActivity

---
+++ app/src/main/java/com/mobdeve/s11/dugang/terencedein/mobdeve_mco/OptionActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.dugang.terencedein.mobdeve_mco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_option)
+    }
+}
