a35fc5fffe224889a6074811e65f47c0f6472124
AkiPunzalan <antonio_punzalan@dlsu.edu.ph>
2022-02-16 02:19:25 +0800

    added sign in button and creating a profile

--- app/src/main/java/com/mobdeve/s11/dugang/terencedein/mobdeve_mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/dugang/terencedein/mobdeve_mco/MainActivity.kt
@@ -5,22 +5,56 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.mobdeve.s11.dugang.terencedein.mobdeve_mco.databinding.ActivityMainBinding
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import android.app.Activity
 
 class MainActivity : AppCompatActivity() {
 
    private lateinit var binding: ActivityMainBinding
 
+   var startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+         result:ActivityResult ->
+
+      if(result.resultCode == Activity.RESULT_OK){
+         var bundle = result.data!!.extras
+
+         Log.i("RESULT", bundle!!.getString("username", "NO NAME PASSED"))
+         Log.i("RESULT", bundle!!.getString("firstname", "NO NAME PASSED"))
+         Log.i("RESULT", bundle!!.getString("lastname", "NO NAME PASSED"))
+         Log.i("RESULT", bundle!!.getString("email", "NO NAME PASSED"))
+         Log.i("RESULT", bundle!!.getString("birthdate", "NO NAME PASSED"))
+
+         var username = bundle.getString("username")
+         binding.username.text = username
+      }
+      else{
+         Snackbar.make(binding!!.root,
+            "UNSUCESSFUL",
+            Snackbar.LENGTH_SHORT).show()
+      }
+   }
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
+      binding = ActivityMainBinding.inflate(layoutInflater)
+      setContentView(binding!!.root)
+
       binding = ActivityMainBinding.inflate(layoutInflater)
       window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
       setContentView(binding!!.root)
 
-      binding.buttonPlay!!.setOnClickListener {
+      binding.btnsignprofile.setOnClickListener {
+         var gotoProfileActivity = Intent(applicationContext, ProfileActivity::class.java)
+         startForResult.launch(gotoProfileActivity)
+      }
+
+      binding.buttonPlay.setOnClickListener {
          val goToGameActivity = Intent(applicationContext, GameActivity::class.java)
          startActivity(goToGameActivity)
 
-         finish()
       }
    }
 }
---
+++ app/src/main/java/com/mobdeve/s11/dugang/terencedein/mobdeve_mco/ProfileActivity.kt
@@ -0,0 +1,55 @@
+package com.mobdeve.s11.dugang.terencedein.mobdeve_mco
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.s11.dugang.terencedein.mobdeve_mco.databinding.ActivityProfileBinding
+
+
+class ProfileActivity : AppCompatActivity() {
+
+   lateinit var binding: ActivityProfileBinding
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      binding = ActivityProfileBinding.inflate(layoutInflater)
+      setContentView(binding!!.root)
+
+      binding.btnsaveprofile.setOnClickListener(){
+         var bundle = Bundle()
+         bundle.putString("username", binding.textusername.text.toString())
+         bundle.putString("firstname", binding.textfirstname.text.toString())
+         bundle.putString("lastname", binding.textlastname.text.toString())
+         bundle.putString("email", binding.textemail.text.toString())
+         bundle.putString("birthdate", binding.textbirthdate.text.toString())
+         var resultIntent = Intent()
+         resultIntent.putExtras(bundle)
+
+         setResult(RESULT_OK, resultIntent)
+         finish()
+      }
+   }
+
+   override fun onSaveInstanceState(outState: Bundle){
+      outState?.run {
+         putString("username", binding!!.textusername.text.toString())
+         putString("firstname", binding!!.textfirstname.text.toString())
+         putString("lastname", binding!!.textlastname.text.toString())
+         putString("email", binding!!.textemail.text.toString())
+         putString("birthdate", binding!!.textbirthdate.text.toString())
+      }
+      super.onSaveInstanceState(outState)
+   }
+
+   override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+      super.onRestoreInstanceState(savedInstanceState)
+
+      savedInstanceState.run {
+         binding!!.textusername.setText(getString("username", ""))
+         binding!!.textfirstname.setText(getString("firstname", ""))
+         binding!!.textlastname.setText(getString("lastname", ""))
+         binding!!.textemail.setText(getString("email", ""))
+         binding!!.textbirthdate.setText(getString("birthdate", ""))
+      }
+   }
+}
